b1cb5e003d2073fa98d7fc249d84d6bf304d6d16
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-24 22:24:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,11 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 
 var addElementToBeginningOfArray = (array,element) =>{
-  
+  array = [element,...array]
+  return array;
+}
+
+var destructivelyAddElementToBeginningOfArray = (array,element) =>{
+  array.unshift(element)
+  return array;
 }
